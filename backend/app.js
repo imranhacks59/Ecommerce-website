a1da2469b335b885fc0cab2d8ee0fc92df9791e7
Md Imran <imranhacks59@gmail.com>
2023-04-19 21:09:04 +0530

    modified backend app.js

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app=express();
-const cookieParser = require("cookie-Parser")
+const cookieParser = require("cookie-Parser");
+const path = require('path');
 const errorMiddleware = require("./middleware/error");
+
 // const cors = require("cors");
 
 
@@ -16,6 +18,15 @@ const user = require("./routes/userRoute");
 app.use("/api/v1",product);
 app.use("/api/v1/",user);
 
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "../frontend/build/index.html"),
+    function (err) {
+      res.status(500).send(err);
+    }
+  );
+});
 //Middleware for error
 
 app.use(errorMiddleware);
